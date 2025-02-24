1d53676b3295bebf89c21fb2c10ec4b6c88005bd
PeruriRohithkumar <rohithkumarperuri@gmail.com>
2025-02-24 15:20:34 +0530

    My Commit Message

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -10,6 +10,6 @@ public class App {
         int b = scan.nextInt();
         int c = scan.nextInt();
 
-        System.out.println("%d\n%d\n%d\n",a,b,c);
+        System.out.printf("%d\n%d\n%d\n",a,b,c);
     }
 }
